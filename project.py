777253aeb388dbb95ab5e8d4d182630bab39fd57
AryaKrisnadi <58764332+AryaKrisnadi99@users.noreply.github.com>
2023-07-14 17:43:55 +0700

    Update project.py

--- project.py
+++ project.py
@@ -69,3 +69,82 @@ if opt == "ICE":
             fig2 = px.line(rate, x="Month", y="Engagement Rate")
             fig2.update_layout(yaxis_title="Followers (%)")
             st.plotly_chart(fig2, use_container_width=True)
+
+else : 
+    #Untuk input data
+    dfins1 = pd.read_csv("data ice.csv", sep=";")
+    dfins2 = pd.read_csv("data ice_2.csv", sep=";")
+    dfins3 = pd.read_csv("data ice_3.csv", sep=";")
+
+    data4 = {
+        'Jumlah Pengikut' :  [78600,18300,83500],
+        'Jumlah Post' : [3617, 3805, 1636],
+       'Perusahaan' : ['ICE', 'JCC', 'JIEXPO']
+    }
+    df4 = pd.DataFrame(data4)
+
+    #membuat dataframe
+    dfins1 = pd.DataFrame(dfins1)
+    dfins2 = pd.DataFrame(dfins2)
+    dfins3 = pd.DataFrame(dfins3)
+
+    #aggregasi data
+    df_merged = pd.concat([dfins1, dfins2, dfins3], ignore_index=True)    
+    
+    #mengubah tipe data tanggal dari object (string) ke dalam tanggal
+    df_merged['Tanggal'] = pd.to_datetime(df_merged['Tanggal'])
+
+    #Pemilihan Grafik
+    grafik_perbandingan = st.selectbox(
+        "Silahkan Pilih Grafik Perbandingan Instagram",
+        ['Engagement Instagram','Like','Komen']
+    )
+    if grafik_perbandingan == 'Like':
+        #Membuat grafik line
+        chart1 = alt.Chart(df_merged).mark_line().encode(
+            alt.X('Tanggal', title='Waktu',axis=alt.Axis(labelAngle=0)),
+            alt.Y('Like', title='Jumlah Like', aggregate='sum'),
+            color='Perusahaan'
+        ).properties(   
+            width=600,
+            height=400
+        )
+
+        #Menampilkan grafik
+        st.subheader('Grafik Perbandingan Jumlah Like Dari ICE JCC dan JIEXPO')
+        st.altair_chart(chart1, use_container_width=True)
+
+    elif grafik_perbandingan == 'Komen' :
+        #Membuat grafik line
+        chart2 = alt.Chart(df_merged).mark_line().encode(
+            alt.X('Tanggal', title='Waktu',axis=alt.Axis(labelAngle=0)),
+            alt.Y('Komentar', title='Jumlah Komen', aggregate='sum'),
+            color='Perusahaan'
+        ).properties(   
+            width=600,
+            height=400
+        )
+
+        #Menampilkan grafik
+        st.subheader('Grafik Perbandingan Jumlah Komen Dari ICE JCC dan JIEXPO')
+        st.altair_chart(chart2, use_container_width=True)
+
+    else : 
+        #Membuat grafik histogram
+        chart3 = alt.Chart(df4).mark_bar().encode(
+            alt.X('Perusahaan', title=None, axis=alt.Axis(labelAngle=0, labels=False)),
+            alt.Y('Jumlah Pengikut', title='Jumlah Pengikut'),
+            color='Perusahaan'
+        )
+        chart4 = alt.Chart(df4).mark_bar().encode(
+            alt.X('Perusahaan', title=None, axis=alt.Axis(labelAngle=0, labels=False)),
+            alt.Y('Jumlah Post', title='Jumlah Post'),
+            color='Perusahaan'
+        )
+
+        #Menampilkan grafik
+        st.subheader('Grafik Jumlah Pengikut berdasarkan Perusahaan')
+        st.altair_chart(chart3, use_container_width=True)
+
+        st.subheader('Grafik Jumlah Post berdasarkan Perusahaan')
+        st.altair_chart(chart4, use_container_width=True)
